72a9ea5ab58ab2c08e45dd7c838afbf6a9e467d9
Danil <danilsmy.edu@gmail.com>
2021-12-18 03:07:59 +0200

    Add few cards buttons generation

--- elfernGame/MyForm.cpp
+++ elfernGame/MyForm.cpp
@@ -1,10 +1,39 @@
 #include "MyForm.h"
 #include <Windows.h>
 using namespace elfernGame; // Название проекта
+using namespace System;
+using namespace System::Drawing;
+using namespace System::ComponentModel;
+using namespace System::Windows::Forms;
+int cardNum;
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 	Application::Run(gcnew MyForm);
 	return 0;
+}
+
+System::Void elfernGame::MyForm::button1_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	Button^ check = gcnew Button();
+	int i = 0;
+	check->BackgroundImage = imageList1->Images[i];
+	check->Size = check->BackgroundImage->Size;
+	this->Controls->Add(check);
+	check->Click += gcnew System::EventHandler(this, &MyForm::check_Click);
+	cardNum = i;
+	Button^ check2 = gcnew Button();
+	check->Location = System::Drawing::Point(25, 0);
+	i = 2;
+	check2->BackgroundImage = imageList1->Images[i];
+	check2->Size = check2->BackgroundImage->Size;
+	this->Controls->Add(check2);
+	check2->Click += gcnew System::EventHandler(this, &MyForm::check_Click);
+	cardNum = i;
+}
+
+System::Void elfernGame::MyForm::check_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	MessageBox::Show("From check", Convert::ToString(cardNum));
 }
--- elfernGame/MyForm.h
+++ elfernGame/MyForm.h
@@ -34,12 +34,17 @@ namespace elfernGame {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::ImageList^ imageList1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::ComponentModel::IContainer^ components;
+	protected:
 
 	private:
 		/// <summary>
 		/// Обязательная переменная конструктора.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -48,12 +53,56 @@ namespace elfernGame {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->components = (gcnew System::ComponentModel::Container());
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->imageList1 = (gcnew System::Windows::Forms::ImageList(this->components));
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(211, 12);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(145, 67);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// imageList1
+			// 
+			this->imageList1->ImageStream = (cli::safe_cast<System::Windows::Forms::ImageListStreamer^>(resources->GetObject(L"imageList1.ImageStream")));
+			this->imageList1->TransparentColor = System::Drawing::Color::Transparent;
+			this->imageList1->Images->SetKeyName(0, L"8_of_clubs.png");
+			this->imageList1->Images->SetKeyName(1, L"8_of_diamonds.png");
+			this->imageList1->Images->SetKeyName(2, L"8_of_hearts.png");
+			this->imageList1->Images->SetKeyName(3, L"8_of_spades.png");
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(301, 122);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"button2";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(584, 395);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e);
+	private: System::Void check_Click(System::Object^ sender, System::EventArgs^ e);
 	};
+	
 }
